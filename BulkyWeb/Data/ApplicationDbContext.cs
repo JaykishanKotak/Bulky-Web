237bf3eefd6e50155fece99de04bae90b34d875b
Jaykishan Kotak <jaykishankotak@gmail.com>
2025-02-12 12:37:05 +0530

    Feat: Initial Commit

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    // DbContext class is root class of entity fw core which  use to connect DB
+    public class ApplicationDbContext : DbContext
+    {
+        // Connection string from appsetting.json
+        // ctor for constructior
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        // db set to craete tab
+        // To create table add-migration in Nugut console and do update-database to apply new migraation
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        // Mapped with db tables - use "prop" shortcut
+        // Key annotation is use to give PK for table
+        // Id or CategoryId Auto considered as PK
+        [Key]
+        public int Id { get; set; }
+        // Required data annotation to give field required property
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+        //PM> add-migration AddCategoryTableToDB
+        // update-database
+
+    }
+}
